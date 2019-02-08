3b7e2dd791986060d5854ce80bbd33c4d6c8c222
FullerAGrace <fuller.a@husky.neu.edu>
2019-02-07 20:05:09 -0500

    JSON

--- loadData.js
+++ loadData.js
@@ -1,10 +1,16 @@
 console.log("Welcome to Data Land!");
-d3.text("data/textData.txt", function(error, data) {
+d3.text("/Data/textData.txt", function(error, data) {
     console.log("error:", error);
     console.log("text:", data);
 });
 
-d3.csv("data/csvData.csv", function(error, data) { 
+d3.csv("/Data/csvData.csv", function(error, data) { 
     console.log("csv:", data);
 
 });
+
+d3.json("file.json", function(error, data) {
+    console.log(data);
+  });
+
+console.log("END OF LINE");
