c43498eea4f4875835661f8f0745d2932122fe45
FullerAGrace <fuller.a@husky.neu.edu>
2019-02-21 13:04:33 -0500

    trying to get d3 to load

---
+++ hw/logdata1.js
@@ -0,0 +1,25 @@
+console.log("Forest area from 1990 SA");
+
+d3.text("data/textData.txt", function(error, data) {
+    console.log("error:", error);
+    console.log("text:", data);
+});
+
+d3.csv("data/csvData.csv", function(error, data) {
+    data.forEach(function(d) {
+        d.export = parseFloat(d.export);
+    });
+    console.log("csv:", data);
+});
+
+d3.json("hw\SAforestarea.json", function(error, data) {
+    console.log("error:", error);
+    console.log("json:", data);
+});
+
+var json = "hw\SAforestarea.json";
+d3.json(json, function(error, data) {
+    console.log("SAforestarea.json", data);
+});
+
+console.log("END OF LINE");
