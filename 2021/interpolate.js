727c2a5c25964eaf27c3d43f46fd6c85f5d478a8
Annalee G. Fuller <fuller.a@northeastern.edu>
2021-04-20 00:08:22 -0400

    updates

--- 2021/interpolate.js
+++ 2021/interpolate.js
@@ -14,8 +14,11 @@ var svg = d3.select("body").append("svg").attr("height","100vh").attr("width","1
 var line = d3.line() //line generator knows it needs to output a line, and then in coming lines, we tell it what to do with the data with respect to the attributes of that line
                 .x(function(d,i){ return d.x*6; }) //for each xy coordinate pair, computes a new x value
                 .y(function(d,i){ return d.y*4; }) //for each xy coordinate pair, computes a new y value
-                .curve(d3.curveCardinal); //built in curve type.  Others available: curveStep, curveCardinal..... [https://github.com/d3/d3/blob/master/API.md]
+                .curve(d3.curveCardinal); //built in curve type.  Others available: curveStep, curveCardinal.....[Curve Explorer: http://bl.ocks.org/d3indepth/b6d4845973089bc1012dec1674d3aff8 ] [https://github.com/d3/d3/blob/master/API.md]
+
 //it's wise to, at least for now, put all of the generators at the top of the code so the browser knows about them (as theyare functions) before it goes forward in constructing the page.                 
+
+//This is where we commit, then, to drawing the path that we just generated using d3.line. 
 svg.append("path").attr("d",line(dataArray))
     .attr("fill","none")
     .attr("stroke","blue");
